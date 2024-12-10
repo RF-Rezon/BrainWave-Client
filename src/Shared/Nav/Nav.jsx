70bc9e6e2244d3e1919b4c69467cf971923c2d99
Iam-Zarif <zarifraja19@gmail.com>
2024-12-10 18:32:12 +0600

    profile

--- src/Shared/Nav/Nav.jsx
+++ src/Shared/Nav/Nav.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { useProfile } from "../../Providers/ProfileProvider";
 
 const Nav = () => {
   const location = useLocation();
+   const { profile } = useProfile();
 
   // Check if current page is an authentication page
   const isAuthPage = [
@@ -26,7 +28,6 @@ const Nav = () => {
 
   const [isScrolled, setIsScrolled] = useState(false);
 
-  // Scroll event listener
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 50) {
@@ -43,7 +44,6 @@ const Nav = () => {
     };
   }, []);
 
-  // Hide Navbar for auth pages
   if (isAuthPage) return null;
 
   return (
@@ -121,25 +121,7 @@ const Nav = () => {
             {/* Profile Icon */}
             <div className="flex items-center justify-center divide-x divide-custom-gray-footer-icon border border-custom-gray-line rounded px-4 py-2">
               <Link to="/profile" className="cursor-pointer pr-2">
-                <svg
-                  className={`w-6 h-6 ${
-                    isScrolled ? "text-custom-base-red" : "text-white"
-                  }`}
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                  />
-                </svg>
+                <img src={profile?.profilePhoto} loading="lazy" className="h-9 w-9 rounded-full" alt="" />
               </Link>
               <div className="pl-2">
                 <svg
